2323306bb71ac3960e72f13df6235f2110aa06ee
big-apple233 <baiyaoming2004@gmail.com>
2025-03-30 19:12:49 +0800

    拼了一些UI
    收集系统UI
    目录UI

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject directoryUI;
+    public void ShowDirectoryUI()
+    { 
+        directoryUI.SetActive(true);
+    }
+    public void HideDirectoryUI()
+    { 
+        directoryUI?.SetActive(false);
+    }
+
+}
